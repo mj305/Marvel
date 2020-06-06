411e286845fb8d071bdc0aa7233605bd213b7e2d
mj305 <maria.j.beckles@gmail.com>
2020-06-06 12:39:39 -0400

    GET API working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import logo from './assets/logo.png';
 import './App.css';
 
 function App() {
+
+  const [query, setQuery] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+    const url = `https://gateway.marvel.com:443/v1/public/characters?apikey=5189968cf45946bfc4dba96d1349fe75`  
+      try {
+        const result = await axios(url);
+        setQuery(result);
+      } catch (error) {
+          console.log("there's error", error)
+      }
+    }
+    fetchData();
+  },[]);
+  console.log(query);
+
   return (
     <div className="App">
       <header className="App-header">
